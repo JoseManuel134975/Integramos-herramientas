01eac40f15fcef1ffd70d917b44f72ef6544f4fa
JoseManuel134975 <josemanuel3606@gmail.com>
2025-03-28 19:06:36 +0100

    Estructura subida + Workflow de prueba + user_data.sh de prueba

---
+++ terraform/user_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+
+sudo docker run -d -p 8080:8080 --name jenkins jenkins/jenkins:lts
